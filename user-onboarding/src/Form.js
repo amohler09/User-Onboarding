47a54ef6deeb3fba39e6dca7ef3bda7f0bcd3185
Amber Mohler <ambermohler@imac.lan>
2019-12-19 18:43:31 -0500

    styled portions of the project, would like to do more if I have time

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -47,7 +47,9 @@ const UserForm = ({values, touched, errors, status}) => {
     return(
         <div className='user-form'>
             <Form>
-                <label htmlFor='name'>Name:</label>
+                <div className='labels'>
+                <h1>Want To Join Our Team?</h1>
+                <label htmlFor='name'>Name:  </label>
                     <Field 
                         id='name'
                         type='text'
@@ -55,8 +57,9 @@ const UserForm = ({values, touched, errors, status}) => {
                         placeholder='Name'
                     />
                     {touched.name && errors.name && (<p className='errors'>{errors.name}</p>)}
+                    <br />
 
-                <label htmlFor='email'>Email:</label>
+                <label htmlFor='email'>Email:  </label>
                     <Field 
                         id='email'
                         type='text'
@@ -64,8 +67,9 @@ const UserForm = ({values, touched, errors, status}) => {
                         placeholder='Email Address'
                     />
                     {touched.email && errors.email && (<p className='errors'>{errors.email}</p>)}
+                    <br />
                 
-                <label htmlFor='password'>Password:</label>
+                <label htmlFor='password'>Password:  </label>
                     <Field
                         id='password'
                         type='password'
@@ -73,11 +77,13 @@ const UserForm = ({values, touched, errors, status}) => {
                         placeholder='Password' 
                     />
                     {touched.password && errors.password && (<p className='errors'>{errors.password}</p>)}
-
+                    <br />
+                    </div>
+                <div className='terms-of-service'>
                 <h2>Terms of Service</h2>
                 <p>Man braid microdosing pour-over chia master cleanse four loko sriracha art party before they sold out salvia retro. Celiac literally pok pok venmo neutra salvia, williamsburg kogi vegan. Ugh sriracha dreamcatcher edison bulb tacos. Hashtag dreamcatcher taxidermy skateboard subway tile coloring book, man bun master cleanse DIY flexitarian chartreuse. Bitters cloud bread keffiyeh live-edge.<br/><br/>
                 Bitters direct trade taxidermy seitan aesthetic chambray tumeric. Subway tile literally single-origin coffee, snackwave chambray adaptogen sustainable. Mumblecore tilde af, portland cardigan authentic jianbing twee pok pok pabst jean shorts locavore meditation hella keytar. Tattooed man bun celiac, truffaut VHS locavore butcher shaman tote bag. Cronut aesthetic poutine, kickstarter pickled hot chicken selvage truffaut heirloom. Skateboard dreamcatcher pabst snackwave. Bicycle rights ugh waistcoat iPhone af jianbing woke portland viral retro mustache drinking vinegar.</p>
-                <h3>Accept Terms of Serive?</h3>
+                <h3>Accept Terms of Service?</h3>
                 <label className='checkbox' htmlFor='terms'>I accept
                     <Field 
                         id='terms'
@@ -85,10 +91,11 @@ const UserForm = ({values, touched, errors, status}) => {
                         name='terms'
                         check={values.terms}
                     />
-                </label>
+                </label><br /><br />
+                </div>
                 {touched.terms && errors.terms && (<p className='errors'>{errors.terms}</p>)}
 
-                <button className='button' type='submit'>Submit</button>
+                <button className='button' type='submit'>Submit</button><br />
             </Form>
             {users.map(user => (
                 <ul key={user.id}>
@@ -113,9 +120,9 @@ const FormikUserForm = withFormik({
     },
         //set up a validationSchema to catch errors when filling out form
     validationSchema: Yup.object().shape({
-        name: Yup.string().required('Wait! This is required!'),
-        email: Yup.string().required('Wait! This is required!'),
-        password: Yup.string().required('Wait! This is required!'),
+        name: Yup.string().required('Wait! Your name is required!'),
+        email: Yup.string().required('Wait! An email is required!'),
+        password: Yup.string().required('Wait! A password is required!'),
         //templates for string validation
         terms: Yup.boolean().oneOf([true], 'Must Accept Terms & Conditions First!')
         //template for checkbox validation
