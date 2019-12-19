999ad761e7c827695013516c923bfd1ac4bf81ec
Amber Mohler <ambermohler@imac.lan>
2019-12-19 17:35:06 -0500

    completed step 3, made a post request for once the submit button is clicked

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -9,6 +9,7 @@ import axios from 'axios';
     //errors - validation errors
     //button - submit button
 
+//What's Needed    
 // Name
 // Email
 // Password
@@ -24,8 +25,10 @@ import axios from 'axios';
 //mapPropstoValues
 //validationSchema
 
-const UserForm = ({values, touched, errors}) => {
+//Step 3.
+//Create a handleSubmit axios post request
 
+const UserForm = ({values, touched, errors}) => {
 
     return(
         <div className='user-form'>
@@ -57,7 +60,11 @@ const UserForm = ({values, touched, errors}) => {
                     />
                     {touched.password && errors.password && (<p className='errors'>{errors.password}</p>)}
 
-                <label className='checkbox' htmlFor='terms'>Terms of Service
+                <h2>Terms of Service</h2>
+                <p>Man braid microdosing pour-over chia master cleanse four loko sriracha art party before they sold out salvia retro. Celiac literally pok pok venmo neutra salvia, williamsburg kogi vegan. Ugh sriracha dreamcatcher edison bulb tacos. Hashtag dreamcatcher taxidermy skateboard subway tile coloring book, man bun master cleanse DIY flexitarian chartreuse. Bitters cloud bread keffiyeh live-edge.<br/><br/>
+                Bitters direct trade taxidermy seitan aesthetic chambray tumeric. Subway tile literally single-origin coffee, snackwave chambray adaptogen sustainable. Mumblecore tilde af, portland cardigan authentic jianbing twee pok pok pabst jean shorts locavore meditation hella keytar. Tattooed man bun celiac, truffaut VHS locavore butcher shaman tote bag. Cronut aesthetic poutine, kickstarter pickled hot chicken selvage truffaut heirloom. Skateboard dreamcatcher pabst snackwave. Bicycle rights ugh waistcoat iPhone af jianbing woke portland viral retro mustache drinking vinegar.</p>
+                <h3>Accept Terms of Serive?</h3>
+                <label className='checkbox' htmlFor='terms'>I accept
                     <Field 
                         id='terms'
                         type='checkbox'
@@ -93,9 +100,21 @@ const FormikUserForm = withFormik({
         //templates for string validation
         terms: Yup.boolean().oneOf([true], 'Must Accept Terms & Conditions First!')
         //template for checkbox validation
-    })
+    }),
 
-
+    //use handleSubmit property to send axios call once submit button is pushed
+    //console.log EVERYTHING
+    handleSubmit(values, { setStatus, resetForm }) {
+        console.log('submitting', values);
+        axios
+            .post('https://reqres.in/api/users', values)
+            .then(response => {
+                console.log('success', response);
+                setStatus(response.data);
+                resetForm();
+            })
+            .catch(error => console.log('Error!', error.response))
+    }
 })(UserForm);
 
 export default FormikUserForm;
